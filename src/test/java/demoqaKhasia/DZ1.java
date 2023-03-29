811e8e608f0d5d9bc5d54a7d0a7086d9627d2ffa
khasia <dihasiya@mail.ru>
2023-03-29 23:55:21 +0400

    added

--- src/test/java/demoqaKhasia/DZ1.java
+++ src/test/java/demoqaKhasia/DZ1.java
@@ -3,6 +3,10 @@ package demoqaKhasia;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DZ1 {
@@ -12,15 +16,41 @@ public class DZ1 {
         Configuration.browserSize = "1920x1080";
         Configuration.browser = "chrome";
 
-        open ("https://demoqa.com/automation-practice-form");
+        String gender = "Male";
+        String birthMonth = "January";
+        String birthYear = "1996";
+        String birthDay = "24";
+        String subjects = "English";
+        String hobbies = "Reading";
+
+
+        open("https://demoqa.com/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
         executeJavaScript("window.scrollBy(0,1000)");
         $("[id=firstName]").setValue("DIMA");
         $("[id=lastName]").setValue("KHASIA");
         $("[id=userEmail]").setValue("dihasiya@mail.ru");
+        $("#genterWrapper").$(byText(gender)).click();
         $("[id=userNumber]").setValue("9999999999");
-        $("[class=subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3]").setValue("Math");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").$(byText(birthYear)).click();
+        $(".react-datepicker__month-select").$(byText(birthMonth)).click();
+        $(".react-datepicker__day--0" + birthDay).click();
+        $("#subjectsContainer input").setValue(subjects).pressEnter();
+        $("#hobbiesWrapper").$(byText(hobbies)).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/DZK.jpg"));
+        $("#currentAddress").setValue("Ukraine, Donetsk, Armavirskaya 21");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Rajasthan")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Jaipur")).click();
+        $("#submit").click();
+
+
+
+
 
     }
 }
